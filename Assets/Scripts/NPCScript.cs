8ecca9ea6e37c8e377d79fe951e317474d52b640
Zync <Lukasjuulnielsen@gmail.com>
2024-06-07 10:43:04 +0200

    Final

--- Assets/Scripts/NPCScript.cs
+++ Assets/Scripts/NPCScript.cs
@@ -117,11 +117,11 @@ public class NPCScript : MonoBehaviour
         AudioSource[] sounds = this.gameObject.GetComponents<AudioSource>();
         while (!hasTalkedWithNPC && Vector3.Distance(this.gameObject.transform.position, Player.transform.position) <= npcSoundDistance)
         {
-            yield return new WaitForSeconds(Random.Range(4f, 5f));
             if(!hasTalkedWithNPC)
             {
                 sounds[Random.Range(0, sounds.Length)].Play();
             }
+            yield return new WaitForSeconds(Random.Range(4f, 5f));
         }
     }
 }
